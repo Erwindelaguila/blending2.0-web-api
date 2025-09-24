bd9bb8fb741738fc14cb82144ba411145025db4d
Víctor Castañeda (TASA) <apoyoprotecso_14@tasa.com.pe>
2025-09-24 00:00:11 -0500

    feat(GetCalParameters): Obtiene los Valores de los Parámetros de Calidad asociados/usados en la ejecución.

---
+++ Function.Blending.Opt/Domain/ReadModels/CalInpParametroItemRm.cs
@@ -0,0 +1,14 @@
+﻿namespace Function.Blending.Opt.Domain.ReadModels;
+
+public sealed record class CalInpParametroItemRm
+{
+  public Guid CalidadId { get; init; }
+  public string? CalidadCodigo { get; init; }
+  public string? CalidadNombre { get; init; }
+  public Guid ParametroId { get; init; }
+  public string? ParametroCodigo { get; set; }
+  public string? ParametroNombre { get; set; }
+  public decimal? Valor { get; init; }
+
+  public CalInpParametroItemRm() { }
+}
---
+++ Function.Blending.Opt/Functions/Triggers/CalEjecucion/GetCalEjecucionParametersFunction.cs
@@ -0,0 +1,48 @@
+﻿using Function.Blending.Opt.Application.Features.CalEjecucion.Queries.GetParameters;
+using Function.Blending.Opt.Functions.Support.Authorization;
+using Function.Blending.Opt.Functions.Support.Execution;
+using Function.Blending.Opt.Functions.Support.Http;
+using Function.Blending.Opt.Functions.Support.ProblemDetails;
+using Function.Blending.Opt.Functions.Support.Routing;
+using Function.Blending.Opt.Shared.Constants;
+using MediatR;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using System.Net;
+
+namespace Function.Blending.Opt.Functions.Triggers.CalEjecucion;
+
+public sealed class GetCalEjecucionParametersFunction(
+  IMediator mediator,
+  IProblemDetailsWriter problem,
+  IRequestContext ctx)
+{
+  [Function(nameof(GetCalEjecucionParametersFunction))]
+  [RequireScopes(ConfigurationKeys.Auth.Scopes.Quality.ReadParameters)]
+  public async Task<HttpResponseData> Run(
+      [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = FunctionRoutes.Quality.GetParameters)]
+      HttpRequestData req,
+      FunctionContext fctx,
+      Guid id)
+  {
+    var result = await mediator.Send(new GetCalEjecucionParametersQuery(id));
+
+    if (!result.IsSuccess || result.Value is null)
+    {
+      return await problem.CreateAsync(
+        fctx,
+        req,
+        HttpStatusCode.NotFound,
+        type: "urn:blending:error:calidad:not-found",
+        title: "Not Found",
+        detail: "Execution not found",
+        extensions: new Dictionary<string, object?>
+        {
+          ["requestedBy"] = ctx.Username,
+          ["executionId"] = id
+        });
+    }
+
+    return await req.OkAsync(result.Value);
+  }
+}
