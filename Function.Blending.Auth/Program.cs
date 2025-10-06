1681e7058ddfe394fde3307142be9c6614cf7d99
Erwindelaguila <erwin.pieers@protecso.com.pe>
2025-10-06 12:38:19 -0500

    chore(ci): ensure cross-platform builds and deploys only run on changed services

--- Function.Blending.Auth/Program.cs
+++ Function.Blending.Auth/Program.cs
@@ -50,7 +50,7 @@ var host = new HostBuilder()
         services.AddApplicationInsightsTelemetryWorkerService();
         services.ConfigureFunctionsApplicationInsights();
         
-        //Deploy v8.0.0
+        //Deploy v9.0.0
     })
     .Build();
 
