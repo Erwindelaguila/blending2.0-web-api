2ddcb897f65fa5f8db389c2d761805d7346baaae
Erwindelaguila <erwin.pieers@protecso.com.pe>
2025-10-06 12:02:59 -0500

    fix: actualzacion .gitlab-ci

--- Function.Blending.Auth/Program.cs
+++ Function.Blending.Auth/Program.cs
@@ -50,7 +50,7 @@ var host = new HostBuilder()
         services.AddApplicationInsightsTelemetryWorkerService();
         services.ConfigureFunctionsApplicationInsights();
         
-        //Deploy v6.0.0
+        //Deploy v7.0.0
     })
     .Build();
 
